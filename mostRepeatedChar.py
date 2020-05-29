888a5b559af0722a08054d5317e7ae334eeca17a
Anna Jose <joann23july@gmail.com>
2020-05-29 15:09:55 -0400

    most repeated char in a string

---
+++ mostRepeatedChar.py
@@ -0,0 +1,12 @@
+sentense = "this is a sentense"
+
+char_frequency = {}
+sentense = sentense.replace(' ', '')
+for char in sentense:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+char_frequency_sorted = sorted(
+    char_frequency.items(), key=lambda kv: kv[1], reverse=True)
+print(char_frequency_sorted[0])
