6f5ecd18a84dc8eb236013b1251bdbd63e05b3ac
Anna Jose <joann23july@gmail.com>
2020-05-28 21:11:01 -0400

    list operations

--- lists.py
+++ lists.py
@@ -46,5 +46,69 @@ alphabets.remove('a')
 del alphabets[0:3]
 letters.clear()
 
-print(alphabets)
+#print(alphabets)
+
+#find
+
+alphabets = ['a','b','c','d']
+#find index of object in a list
+#print(alphabets.index('b'))
+
+#if not in list
+#print(alphabets.count('e'))
+
+if "e" in alphabets:
+    print(alphabets.index("e"))
+
+#sorting
+
+numbers = [3,7,90,43,12,55]
+print(sorted(numbers))
+print(sorted(numbers, reverse=True)) #reverse
+
+#sorting complex items
+
+items = [
+    ("apple", 10),
+    ("banana", 20),
+    ("pineapple", 40),
+    ("water melon", 5)
+]
+
+# def sort_item(item):
+#     return item[1]
+
+# items = items.sort(key=sort_item)
+# print(items)
+
+items.sort(key=lambda item: item[1])
+#print(items)
+
+#MAP FUNCTION
+prices = list(map(lambda item: item[1], items))
+print(prices)
+
+#FILTER FUNCTION
+filtered = list(filter(lambda item: item[1] > 10, items))
+print(filtered)
+
+#LIST COMPREHENSION
+#MAP
+items = [item[1] for item in items]
+print(items)
+#FILTER
+#filtered = [item for item in items if item[1]>=10]
+#print(filtered)
+
+#ZIP FUNCTION - MAkE TUPLE - MULTIPLE LIST COmbine
+
+list1 = [1,2,3,4]
+list2 = [10,20,30,40]
+
+print(list(zip(list1,list2)))
+
+print(list(zip("abcd", list1, list2)))
+
+
+
 
